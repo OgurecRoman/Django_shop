f32f2cca59c5acf4f5693926cb2965c52f20c12c
OgurecRoman <treninasonya@yandex.ru>
2024-02-07 17:55:14 +0300

    first example DONE

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-super-secret-key"
+SECRET_KEY = "django-secret-key"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
