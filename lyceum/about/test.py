316082917b9e531484189c797d46b5f9d3865b5e
OgurecRoman <treninasonya@yandex.ru>
2024-02-15 00:46:43 +0300

    fixed lines

--- lyceum/about/test.py
+++ lyceum/about/test.py
@@ -2,7 +2,6 @@ from django.test import Client, TestCase
 
 
 class AboutStaticURLTests(TestCase):
-
     def test_about_endpoint(self):
         response = Client().get("/about/")
         self.assertEqual(response.status_code, 200)
--- lyceum/catalog/test.py
+++ lyceum/catalog/test.py
@@ -2,7 +2,6 @@ from django.test import Client, TestCase
 
 
 class CatalogStaticURLTests(TestCase):
-
     def test_catalog_endpoint(self):
         response = Client().get("/catalog/")
         self.assertEqual(response.status_code, 200)
