fa8f61f045e0ecbe3301ba68b5b61ac30c9e3961
OgurecRoman <treninasonya@yandex.ru>
2024-02-16 22:54:56 +0300

    fixed reverse middleware

--- lyceum/lyceum/tests.py
+++ lyceum/lyceum/tests.py
@@ -1,11 +1,19 @@
-from django.test import Client, TestCase
-
-contents = []
+from django.test import Client, TestCase, override_settings
 
 
 class CatalogStaticURLTests(TestCase):
-    def test_catalog_endpoint(self):
+    @override_settings(ALLOW_REVERSE=True)
+    def test_catalog_endpoint_true(self):
+        contents = []
         for i in range(10):
             response = Client().get("/coffee/")
             contents.append(response.content.decode("utf-8"))
         self.assertEqual(contents.count("Я кинйач"), 1)
+
+    @override_settings(ALLOW_REVERSE=False)
+    def test_catalog_endpoint_false(self):
+        contents = []
+        for i in range(10):
+            response = Client().get("/coffee/")
+            contents.append(response.content.decode("utf-8"))
+        self.assertNotIn("Я кинйач", contents)
