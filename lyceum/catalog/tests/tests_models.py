b3bd6863b730a8c10fdabb517d99fb2e29760d97
OgurecRoman <treninasonya@yandex.ru>
2024-02-28 11:05:02 +0300

    add canonical_name

--- lyceum/catalog/tests/tests_models.py
+++ lyceum/catalog/tests/tests_models.py
@@ -1,67 +1,8 @@
-import itertools
-
 import django.core.exceptions
 import django.test
-from django.test import Client, TestCase
 import parameterized
 
 import catalog.models
-
-
-class CatalogStaticURLTests(TestCase):
-    def test_catalog_endpoint(self):
-        response = Client().get("/catalog/")
-        self.assertEqual(response.status_code, 200)
-
-    @parameterized.parameterized.expand(
-        [
-            ("1", 200),
-            ("100", 200),
-            ("0", 200),
-            ("-0", 404),
-            ("-100", 404),
-            ("0.5", 404),
-            ("abc", 404),
-            ("0abc", 404),
-            ("abc0", 404),
-            ("$%^`", 404),
-            ("1e5", 404),
-        ],
-    )
-    def test_catalog_item_endpoint(self, url, expected_status):
-        response = django.test.Client().get(f"/catalog/{url}/")
-        self.assertEqual(response.status_code, expected_status)
-
-    @parameterized.parameterized.expand(
-        [
-            (x[0], x[1][0], x[1][1])
-            for x in list(
-                itertools.product(
-                    [
-                        "converter",
-                        "re",
-                    ],
-                    [
-                        ("1", 200),
-                        ("100", 200),
-                        ("0", 404),
-                        ("-0", 404),
-                        ("-100", 404),
-                        ("0.5", 404),
-                        ("abc", 404),
-                        ("0abc", 404),
-                        ("abc0", 404),
-                        ("$%^`", 404),
-                        ("1e5", 404),
-                    ],
-                ),
-            )
-        ],
-    )
-    def test_catalog_conv_endpoint(self, prefix, url, expected_status):
-        full_url = f"/catalog/{prefix}/{url}/"
-        response = Client().get(full_url)
-        self.assertEqual(response.status_code, expected_status)
 
 
 class ModelsTests(django.test.TestCase):
@@ -182,4 +123,29 @@ class ModelsTests(django.test.TestCase):
         self.assertEqual(
             catalog.models.Category.objects.count(),
             category_count + 1,
+        )
+
+    @parameterized.parameterized.expand(
+        [
+            ("тестовая категория 1",),
+            ("Тестовая категория 1.",),
+            ("!Тестовая категория 1!",),
+            ("Тестовая категория1",),
+            ("тестовая категория 1,",),
+        ],
+    )
+    def test_category_negative_name(self, name):
+        category_count = catalog.models.Category.objects.count()
+
+        with self.assertRaises(django.core.exceptions.ValidationError):
+            category = catalog.models.Category(
+                name=name,
+                slug="testslug",
+            )
+            category.full_clean()
+            category.save()
+
+        self.assertEqual(
+            catalog.models.Category.objects.count(),
+            category_count,
         )
