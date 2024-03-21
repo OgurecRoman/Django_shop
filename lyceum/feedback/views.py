92df139dae91e8f51151bfc529b576b153fec62c
OgurecRoman <treninasonya@yandex.ru>
2024-03-21 18:35:52 +0300

    change name to feedback_form

--- lyceum/feedback/views.py
+++ lyceum/feedback/views.py
@@ -9,12 +9,12 @@ import lyceum.settings
 
 def feedback(request):
     template = "feedback/feedback.html"
-    form = feedback_forms.FeedbackForm(request.POST or None)
-    if request.method == "POST" and form.is_valid():
-        name = form.cleaned_data["name"]
-        text = form.cleaned_data["text"]
+    feedback_form = feedback_forms.FeedbackForm(request.POST or None)
+    if request.method == "POST" and feedback_form.is_valid():
+        name = feedback_form.cleaned_data["name"]
+        text = feedback_form.cleaned_data["text"]
         mail_from = lyceum.settings.DJANGO_MAIL
-        mail_to = form.cleaned_data["mail"]
+        mail_to = feedback_form.cleaned_data["mail"]
         django.core.mail.send_mail(
             "Заголовок",
             f"Привет, {name}\n{text}",
@@ -25,7 +25,7 @@ def feedback(request):
             fail_silently=True,
         )
         feedback_item = feedback_models.Feedback.objects.create(
-            **form.cleaned_data,
+            **feedback_form.cleaned_data,
         )
         feedback_item.save()
 
@@ -39,7 +39,7 @@ def feedback(request):
         )
 
     context = {
-        "form": form,
+        "feedback_form": feedback_form,
     }
     return django.shortcuts.render(request, template, context)
 
